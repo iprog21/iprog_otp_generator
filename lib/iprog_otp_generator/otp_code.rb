93ac98fac17dd043b11f91b50731755eaefd2e6d
iprog21 <jaysonpresto.iprog21@gmail.com>
2024-10-15 23:54:09 +0800

    Feature: add column otp code confirmed

--- lib/iprog_otp_generator/otp_code.rb
+++ lib/iprog_otp_generator/otp_code.rb
@@ -8,20 +8,27 @@ module IprogOtpGenerator
    end while self.class.exists?(otp_code: otp_code)
 
    self.otp_code_expires_at = Time.now + 5.minutes
-
-   save!
+   self.otp_code_confirmed  = false
+   
+   save(validate: false)
   end
 
   def valid_otp_code?(code = nil)
    return false unless otp_code && otp_code_expires_at
 
-   otp_code == code && otp_code_expires_at > Time.now
+   if otp_code == code && otp_code_expires_at > Time.now
+    update_columns otp_code_confirmed: true
+   end
+
+   otp_code_confirmed
   end
 
   def clear_otp_code
-   self.otp_code = self.otp_code_expires_at = nil
+   self.otp_code            = nil 
+   self.otp_code_expires_at = nil
+   self.otp_code_confirmed  = false
 
-   save!
+   save(validate: false)
   end
  end
 end
