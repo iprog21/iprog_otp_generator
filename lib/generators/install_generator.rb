f1e00c9a32c5de701a7090ec9f65269d3fe6e3bc
iprog21 <jaysonpresto.iprog21@gmail.com>
2024-10-19 00:01:44 +0800

    Remove migration template file and switch to manual copying and Add byebug in development mode

--- lib/generators/install_generator.rb
+++ lib/generators/install_generator.rb
@@ -1,24 +1,39 @@
 # frozen_string_literal: true
 require 'rails/generators'
-require 'rails/generators/migration'
 
 module IprogOtpGenerator
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      include Rails::Generators::Migration  # Place it here to include migration support
-
-      source_root File.expand_path('../', __dir__)  # Make sure this points to your templates folder
+      source_root File.expand_path('..', __FILE__) # Make sure this points to your templates folder
 
       argument :model_name, type: :string, default: "User", banner: "MODEL"
 
       desc "Creates a migration to add OTP fields to the specified model's table"
 
       def copy_migration
-        model      = model_name.constantize
-        table_name = model.table_name
-
-        migration_template "migration.rb.tt", "db/migrate/add_otp_fields_to_#{table_name}.rb", table_name: table_name, migration_version: migration_version
+        # Define table name based on the model name
+        table_name = model_name.tableize
+      
+        # Generate the migration file name
+        timestamp = Time.now.utc.strftime("%Y%m%d%H%M%S")
+        migration_filename = "db/migrate/#{timestamp}_add_otp_fields_to_#{table_name}.rb"
+      
+        # Define the content of the migration file as a string
+        migration_content = <<-RUBY
+            class AddOtpFieldsTo#{table_name.camelize} < ActiveRecord::Migration#{migration_version}
+              def change
+                add_column :#{table_name}, :otp_code, :string
+                add_column :#{table_name}, :otp_code_expires_at, :datetime
+                add_column :#{table_name}, :otp_code_resend_interval, :integer, default: 60
+                add_column :#{table_name}, :otp_code_confirmed, :boolean, default: false
+              end
+            end
+         RUBY
+      
+        # Create the migration file manually
+        create_file migration_filename, migration_content
       end
+      
 
       # This method is required by Rails generators to create a unique timestamp for migration files
       def self.next_migration_number(dirname)
